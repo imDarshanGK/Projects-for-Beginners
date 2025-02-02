d0d500cff4150358c6fb9f4133e405d57f784a75
imDarshanGK <darshan241210s@gmail.com>
2025-02-02 14:42:37 +0530

    feat: Add task management script for to-do list

---
+++ To-Do List/To-Do List.py
@@ -0,0 +1,19 @@
+tasks = []
+
+while True:
+    print("\nTo-Do List:")
+    for i, task in enumerate(tasks, 1):
+        print(f"{i}. {task}")
+
+    action = input("\nAdd, Remove, or Quit? ").lower()
+    if action == "add":
+        task = input("Enter a task: ")
+        tasks.append(task)
+    elif action == "remove":
+        task_num = int(input("Enter task number to remove: "))
+        if 1 <= task_num <= len(tasks):
+            tasks.pop(task_num - 1)
+        else:
+            print("Invalid task number!")
+    elif action == "quit":
+        break
