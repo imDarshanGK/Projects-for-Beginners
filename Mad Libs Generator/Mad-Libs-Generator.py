2d442a42adcd766e9cec5998c2fe1d8327761aee
imDarshanGK <darshan241210s@gmail.com>
2025-02-02 14:49:58 +0530

    feat: Add interactive story generator script

---
+++ Mad Libs Generator/Mad-Libs-Generator.py
@@ -0,0 +1,7 @@
+noun = input("Enter a noun: ")  
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+place = input("Enter a place: ")
+
+story = f"Once upon a time, there was a {adjective} {noun} who loved to {verb} in {place}."
+print(story)
